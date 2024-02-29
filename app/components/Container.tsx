177bbef74118b38f5dc3a098220249ee8019b7df
MatiScavuzzo <mati.ntvg@hotmail.com>
2024-02-29 16:56:22 -0300

    Estados y errores

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,17 +1,39 @@
 'use client'
 
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 import { Arrow } from "./Arrow"
-import { DateGrid } from "./Date-grid"
+import { DateGrid, DayError } from "./Date-grid"
 import { Result } from "./Result"
 
 export const Container = () => {
   const [years, setYears] = useState<string>('--')
   const [months, setMonths] = useState<string>('--')
   const [days, setDays] = useState<string>('--')
+  const [dayError, setDayError] = useState<string>('')
+  const [monthError, setMonthError] = useState<string>('')
+  const [yearError, setYearError] = useState<string>('')
+
+  const handleDay = (e: ChangeEvent<HTMLInputElement>) => {
+    const day = e.target.value;
+    console.log(day, DayError.empty)
+    if (day < e.target.min || day > e.target.max) {
+      setDayError(DayError.invalid)
+    } else if (day.length === 0) {
+      setDayError(DayError.empty)
+    } else {
+      console.log(typeof day)
+    }
+  }
+  const handleMonth = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+  }
+  const handleYear = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+  }
+
   return (
     <div className='flex flex-col items-center gap-2 justify-around text-black bg-white w-full h-96 rounded-t-lg rounded-bl-lg rounded-br-3xl'>
-      <DateGrid/>
+      <DateGrid onDay={handleDay} dayError={dayError} onMonth={handleMonth} monthError={monthError} onYear={handleYear} yearError={yearError} />
       <Arrow />
       <Result years={years} months={months} days={days} />
     </div>
--- app/components/Date-grid.tsx
+++ app/components/Date-grid.tsx
@@ -1,17 +1,39 @@
-export const DateGrid = () => {
+import { ChangeEvent } from "react"
+
+export enum DayError {
+  'empty' = 'The field is required',
+  'invalid' = 'Must be a valid day',
+}
+
+export const DateGrid = ({ 
+  onDay,
+  onMonth,
+  onYear,
+  dayError,
+  monthError,
+  yearError, 
+}: { 
+  onDay: (e: ChangeEvent<HTMLInputElement>) => void,
+  onMonth: (e: ChangeEvent<HTMLInputElement>) => void,
+  onYear: (e: ChangeEvent<HTMLInputElement>) => void,
+  dayError: string,
+  monthError: string,
+  yearError: string,
+}) => {
+  const getYear = new Date().getFullYear()
   return (
     <section className='grid grid-cols-3 gap-2 p-4'>
       <div className='flex flex-col gap-1'>
         <p className="text-smokey-grey text-xs font-bold">DAY</p>
-        <input className="border border-light-grey p-3 rounded-md text-xl font-extrabold " type='text' name='day' id='day' />
+        <span className={`${dayError != '' ? dayError === DayError.empty ? 'after:content-["The field is required"] after:text-xs after:text-light-red after:italic' : `after:content-["${DayError.invalid}"] after:text-xs after:text-light-red after:italic` : ''}`}><input onChange={onDay} min={1} max={31} className={`${dayError ? 'border-light-red' : 'border-light-grey'} w-full border p-3 rounded-md text-xl font-extrabold remove-arrow`} type='number' name='day' id='day' /></span>
       </div>
       <div className='flex flex-col gap-1'>
         <p className="text-smokey-grey text-xs font-bold">MONTH</p>
-        <input className="border border-light-grey p-3 rounded-md text-xl font-extrabold " type='text' name='month' id='month' />
+        <input onChange={onMonth} min={1} max={12}  className="border border-light-grey p-3 rounded-md text-xl font-extrabold remove-arrow" type='number' name='month' id='month' />
       </div>
       <div className='flex flex-col gap-1'>
         <p className="text-smokey-grey text-xs font-bold">YEAR</p>
-        <input className="border border-light-grey p-3 rounded-md text-xl font-extrabold " type='text' name='year' id='year' />
+        <input onChange={onYear} min={1900} max={getYear} className="border border-light-grey p-3 rounded-md text-xl font-extrabold remove-arrow" type='number' name='year' id='year' />
       </div>
     </section>
   )
